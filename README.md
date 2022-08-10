72257668a2b0913cc7e31dfa152ffe462ed1d3c6
attribbuttes <electronic.circus@gmail.com>
2022-08-09 21:42:10 -0300

    sprint2

--- README.md
+++ README.md
@@ -17,4 +17,16 @@ la triestina
 4. 
 wireframes ok en carpeta
 
+9/8/2022
+se comienza con el sprint 2
+HACER TABLERO DE TRABAJO
 
+Resumen de entregables
+★ Archivo retro.md con el resultado de la retrospectiva.
+★ Enlace al tablero de trabajo en el archivo README.md.
+★ Aplicación Node.js+Express con:
+○ Home (index.html)
+○ Detalle del producto (productDetail.html)
+○ Carrito de compras (productCart.html)
+○ Formulario de registro (register.html)
+○ Formulario de login (login.html)
--- app.js
+++ app.js
@@ -9,6 +9,38 @@ app.get('/', (req,res)=>{
     res.sendFile((path.join(__dirname, 'views', 'index.html')))
 })
 
+app.get('/account', (req,res)=>{
+    res.sendFile((path.join(__dirname, 'views', 'account.html')))
+})
+
+app.get('/products', (req,res)=>{
+    res.sendFile((path.join(__dirname, 'views', 'products.html')))
+})
+
+app.get('/armasCortas', (req,res)=>{
+    res.sendFile((path.join(__dirname, 'views', 'armasCortas.html')))
+})
+
+app.get('/armasLargas', (req,res)=>{
+    res.sendFile((path.join(__dirname, 'views', 'armasLargas.html')))
+})
+
+app.get('/camping', (req,res)=>{
+    res.sendFile((path.join(__dirname, 'views', 'camping.html')))
+})
+
+app.get('/carrito', (req,res)=>{
+    res.sendFile((path.join(__dirname, 'views', 'carrito.html')))
+})
+
+app.get('/detalleProd', (req,res)=>{
+    res.sendFile((path.join(__dirname, 'views', 'detalleProd.html')))
+})
+
+app.get('/registerlogin', (req,res)=>{
+    res.sendFile((path.join(__dirname, 'views', 'registerlogin.html')))
+})
+
 app.listen(3080, (req,res)=>{
     console.log('pagina online')
 })
